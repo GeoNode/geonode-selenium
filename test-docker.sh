2c6e8167f98ff4ef4591b40cccce647250ae94b7
Francesco Frassinelli <francesco.frassinelli@nina.no>
2019-04-02 10:53:40 +0200

    Custom docker-compose options

--- test-docker.sh
+++ test-docker.sh
@@ -9,11 +9,14 @@ cd $(dirname "${BASH_SOURCE[0]}")
 if [ -z "$GEONODE_REPOSITORY" ]; then
     GEONODE_REPOSITORY="geonode"
 fi
+if [ -z "$COMPOSE_OPTS" ]; then
+    COMPOSE_OPTS="--build"
+fi
 
 for i in {1..3}; do
     cd "$GEONODE_REPOSITORY/scripts/spcgeonode/"
     docker-compose -f docker-compose.yml down --volumes
-    docker-compose -f docker-compose.yml up -d --build \
+    docker-compose -f docker-compose.yml up -d $COMPOSE_OPTS \
         django geoserver postgres nginx \
         celery celerybeat celerycam rabbitmq
     cd -
