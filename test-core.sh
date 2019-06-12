f811971ed200ff116cd412f637acf097e1d09f82
Francesco Frassinelli <francesco.frassinelli@nina.no>
2019-06-12 12:57:45 +0200

    Simplify test-core.sh

--- test-core.sh
+++ test-core.sh
@@ -12,17 +12,13 @@ set +a
 
 cd $(dirname "${BASH_SOURCE[0]}")
 for i in {1..3}; do
-
-    log=$(docker inspect -f '{{.LogPath}}' django4geonode 2> /dev/null)
-    sudo truncate -s 0 $log
-
     cd "$GEONODE_REPOSITORY/"
     docker-compose -f docker-compose.yml -f docker-compose.override.localhost.yml down --volumes
     docker-compose -f docker-compose.yml -f docker-compose.override.localhost.yml up -d $COMPOSE_OPTS
     cd -
 
     for i in {1..60}; do
-        if [ $(docker logs --tail 10 django4geonode 2>&1 | grep -c "getting INI configuration from /usr/src/app/uwsgi.ini") -ne 0 ]; then
+        if docker logs --tail 10 django4geonode |& grep "getting INI configuration from /usr/src/app/uwsgi.ini"; then
             ./test.sh "$@"
             exit $?
         fi
